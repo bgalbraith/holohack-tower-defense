585d709980b5d32b560b04f305e92a5667b431cb
Andrew Moran <andrewmo2014@gmail.com>
2016-06-11 04:39:54 -0700

    Dev scene

---
+++ HoloHack Tower Defense/Assets/Scripts/MinionPathFinder.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionPathFinder : MonoBehaviour {
+
+    private Vector3 spawnPointPos;
+    private Vector3 endPointPos;
+
+    private Vector3 startDirection;
+
+    public Transform contactPoint;
+    public bool onWall = false;
+
+    public float TARGET_DIST_EPSILON = 0.5f;
+    public float WALL_DIST_EPSILON = 0.05f;
+
+
+    void Awake()
+    {
+        spawnPointPos = GameObject.Find("SpawnPoint").transform.position;
+        endPointPos = GameObject.Find("EndPoint").transform.position;
+
+        Vector3 direction = endPointPos - spawnPointPos;
+        direction.y = 0.0f;
+        startDirection = direction;
+    }
+
+	// Use this for initialization
+	void Start () {
+        transform.forward = startDirection;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        Vector3 direction = endPointPos - contactPoint.position;
+        Vector3 heading = direction;
+        heading.y = 0.0f;
+        
+        if (onWall)
+        {
+            if (Vector3.Distance(endPointPos, contactPoint.position) < TARGET_DIST_EPSILON)
+            {
+                Destroy(gameObject);
+            }
+        }
+        else
+        {
+            if (heading.sqrMagnitude < WALL_DIST_EPSILON)
+            {
+                transform.GetComponent<Rigidbody>().useGravity = false;
+                transform.forward = transform.up;
+                onWall = true;
+            }
+        }
+
+        
+
+        
+
+    }
+
+}
---
+++ HoloHack Tower Defense/Assets/Scripts/MinionSpawner.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MinionSpawner : MonoBehaviour {
+
+    public Transform spawnPoint;
+    public Transform minionPrefab;
+    public float delay = 1.0f;
+
+    public bool active = true;
+
+    // Use this for initialization
+    IEnumerator Start () {
+        while (active)
+        {
+            yield return new WaitForSeconds(1);
+            Instantiate(minionPrefab, spawnPoint.position, Quaternion.identity);
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
