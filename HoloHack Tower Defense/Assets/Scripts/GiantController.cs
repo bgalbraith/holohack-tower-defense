bcf8aaf2eec87759fc20839840c72b7f0998ba33
Byron Galbraith <byron.galbraith@gmail.com>
2016-06-11 15:54:29 -0400

    basic giant scene

---
+++ HoloHack Tower Defense/Assets/Scripts/GiantController.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GiantController : MonoBehaviour {
+
+    public bool DemoMode = true;
+
+    Animator animator;
+    float elapsed;
+    string[] actions = new string[] {"hit1", "hit2", "death", "punchRight", "punchLeft", "slamRight", "slamLeft", "slamBig", "downGrab", "downLook"};
+
+    // Use this for initialization
+	void Start()
+    {
+        animator = gameObject.GetComponentInChildren<Animator>();
+        elapsed = 0f;
+	}
+	
+	// Update is called once per frame
+	void Update()
+    {
+        elapsed += Time.deltaTime;
+        if (elapsed >= 5.0f)
+        {
+            elapsed = 0f;
+            int i = Random.Range(0, 9);
+            animator.SetTrigger(actions[i]);
+        }
+	}
+}
